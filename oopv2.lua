1ce2798e8cce8847cda6dc6392c07c36cbe4f9ad
Callum Brieske <callum@nsl.co.nz>
2019-05-06 19:57:43 +1200

    Progress on v2 framework

--- oopv2.lua
+++ oopv2.lua
@@ -36,14 +36,67 @@ function plugin:code()
     print("Wow! Our Runtime code works!")
 end
 
+framework = {   -- Framework boilerplate & inheritance methods.
+
+    _metatable = {
+
+        immutableLocally = {},
+
+        immutableDownstream = {},
+
+        immutableGlobally = {
+
+            inherit = function(self, localData, immutableLocally, immutableDownstream, immutableGlobally, immutableRootKey)
+                localData = type(localData) == "table" and localData or {}    -- Ensure that we have a table for the local data.
+
+                localData._metatable = {
+                    immutableLocally = setmetatable(type(immutableLocally) == "table" and immutableLocally or {}, {__index = self._metatable.immutableLocally}),
+                    immutableDownstream = setmetatable(type(immutableDownstream) == "table" and immutableDownstream or {}, {__index = self._metatable.immutableDownstream}),
+                    immutableGlobally = self._metatable.immutableGlobally,  --setmetatable(type(immutableGlobally) == "table" and immutableGlobally or {}, {__index = self._metatable.immutableGlobally}),
+
+                    __index = function(t, k)
+                        print("__index", k)
+                        return rawget(self, k) or t._metatable.immutableLocally[k] or t._metatable.immutableDownstream[k] or t._metatable.immutableGlobally[k]
+                    end,
+
+                    __newindex = function(t, k, v)
+                        print("__newindex", k)
+                        if rawget(t._metatable.immutableLocally, k) ~= nil then
+                            error("Attempt to change locally immutable key.", 2)
+                        elseif rawget(t._metatable.immutableDownstream, k) ~= nil then
+                            error("Attempt to change upstream immutable key.", 2)
+                        elseif t._metatable.immutableGlobally[k] ~= nil then
+                            error("Attempt to change globally immutable key.", 2)
+                        elseif immutableRootKey then
+                            error("Attempt to change globally immutable root key.", 2)
+                        else
+                            rawset(t, k, v)
+                        end
+                    end,
+                }
+
+                if type(immutableGlobally) == "table" then
+                    for k, v in pairs(immutableGlobally) do
+                        if localData._metatable.immutableGlobally[k] then
+                            error("Attempt to change globally immutable root key.", 2)
+                        end
+                        localData._metatable.immutableGlobally[k] = v   -- Copy globals to global metatable.
+                    end
+                end
+
+                return setmetatable(localData, localData._metatable)
+            end,
+        },
+
+    },
+
+}
+framework = framework._metatable.immutableGlobally.inherit(framework, nil, nil, nil, framework, true)    -- Make framework immutable.
 
 
-
-
-
-
-
-
+--t = framework:inherit({c = "tr"}, {a = "a"}, {b = "c"})
+t = framework:inherit({a = "a"},{b = "b"},{c = "c"})
+d = t:inherit(nil, nil, {c = "see!"})
 
 
 
